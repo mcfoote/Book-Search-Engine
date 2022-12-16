4abcf59d67aa1632c39ca0cac91e04877c55f33a
Mitchell Foote <mcfoote321@gmail.com>
2022-12-15 20:47:55 -0500

    work on shemas

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -2,25 +2,39 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login():
+        addUser():
+        saveBook():
+        removeBook(bookId): User
+    }
+
     type User {
         _id: ID
         username: String
         email: String
-        password: String
+        bookCount: Int
         savedBooks: [Book]
     }
 
     type Book {
         _id: ID
+        bookId: String
         authours: String
         description: String
-        bookId: String
+        title: String
         image: String
         link: String
-        title: String
     }
 
-    
+    type Auth {
+        token: ID!
+        user: User
+    }
 
 `;
 
